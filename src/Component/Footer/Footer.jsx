8a1a706e0a6716f123d3c11f5fbf4bbb9238af1f
monti <montijha143@gmail.com>
2025-09-16 18:20:49 +0530

    footer push

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Footer.css'
+import Wave from '../../img/wave.png';
+import { UilInstagram, UilFacebook, UilGithub } from '@iconscout/react-unicons';
+// import Insta from '@iconscout/react-unicons/icons/uil-instagram'
+// import Facebook from '@iconscout/react-unicons/icons/uil-facebook'
+// import Github from '@iconscout/react-unicons/icons/uil-github'
+const Footer = () => {
+  return (
+    <div className="footer">
+        <img src={Wave}  alt="" style={{width: '100%'}}/>
+        <div className="f-content">
+            <span>montijha143@gmail.com</span>
+            <div className="f-icons">
+                <UilInstagram color='white' size='3rem'/>
+                <UilFacebook color='white' size='3rem'/>
+                <UilGithub color='white' size='3rem'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
